0c509f0b031051e8885a5f08392a2098943ac35f
Ajay <ajayc0739@gmail.com>
2022-05-26 22:32:03 +0530

    new

---
+++ fileupload.js
@@ -0,0 +1,25 @@
+var fileupload = require('formidable');
+var http = require('http')
+var fs = require('fs');
+
+
+http.createServer(function(req,res){
+    if(req.url=='/fileupload'){
+        var form = new fileupload.IncomingForm();
+        form.parse(req, function(err,fields,files){
+            var oldpath = files.filetoupload.fileupload;
+            var newpath = 'C:/Users/Your Name/' + files.filetoupload.originalFilename;
+            res.write('File Uploaded');
+            res.end();
+        });
+    }
+    else{
+        res.writeHead(200 , {'Content-Type':'text/html' });
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="fileupload"> <br>');
+        res.write('<input type="submit" >');
+        res.write('</form>');
+        return res.end();
+
+    }
+}).listen(8080);
